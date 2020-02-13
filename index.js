c0105be2eb95b3145e163c76cb5263e7003cf095
Daniel <vermont.daniel.johnson@gmail.com>
2020-02-12 22:57:59 -0500

    set up rooms to be completed later, organized a little

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ let states = {
 let currentState = "green";
 
 
-//list of valid string inputs
+//list of valid string inputs---------------------------------------
+const entryAnswer = ["read sign", "read", "look"]
 
 
-
-//list of functions
+//list of functions-------------------------------------------------
 function checkStatus(){
 
 }
@@ -36,16 +36,24 @@ function enterState(newState) {
   }
 }
 
+function sanitizeString(string) {
+  string = string
+    .toString()
+    .trim()
+    .toLowerCase();
+
+  return string;
+}
 
 
-//list of classes
+//list of classes---------------------------------------------------
 class Room {
     constructor(north, east, south, west, roomInventory, lock){
         this.north = north;
         this.east = east;
         this.south = south;
         this.west = west;
-        this.roomInventory = roomInventory;
+        this.roomInventory = roomInventory || [];
         this.lock = false;
         this.move = function(room){
             currentRoom = room
@@ -54,31 +62,47 @@ class Room {
 }
 
 
-//list of rooms
-let startRoom = new Room (null, null, /*entry to nextroom */ null,  )
+//list of rooms-----------------------------------------------------
 
+//connects: centerRoom(south), has three items and is locked.
+let startRoom = new Room (null, null, /*centerRoom,*/ null, ['statue', 'north painting', 'sign by door'], true )
 
-//list of objects (if need)
+//connects to 4 rooms, no items, is unlocked
+let centerRoom = new Room (/*startRoom, hallwayRoom, itemRoom, trapRoom,*/ [])
+
+//connects: centerRoom(west) and finalRoom(south), has puzzle, is locked, needs key(itemRoom) to unlock
+let hallwayRoom = new Room (null, null, /*finalRoom, centerRoom*, [inventory: needs interactive puzzle*/ true )
+
+//needs better name, connects: centerRoom(north), has 2 keys, and three puzzle pieces, is unlocked
+let itemRoom = new Room (/*centerRoom,*/ null, null,  null, ['hallwayRoom key', 'trapRoom key', 'puzzle 1', 'puzzle 2', 'puzzle 3'] )
+
+//connects: centerRoom(east), no items, needs key(itemRoom) to unlock, if entered should console.log losing method && change status
+let trapRoom = new Room (null, /*centerRoom,*/ null, null, [], true)
+
+//connects: hallwayRoom(north), no items, hallwayRoom puzzle unlocks, if entered console.log victory message
+let finalRoom = new Room (/*hallwayRoom,*/ null, null, null, [], true)
+
+//list of objects (if need)-----------------------------------------
+
+//Player Information------------------------------------------------
 let player = {
     playerInventory : null,
     currentRoom : null,
 
 }
 
-//Player Information
-
 
 start ()
 
 async function start(){
-    
+//Start up message
 console.log("You realize you are in a dark, dingy and smelly room.  You don't know how you got here, and frankly don't even remember your name!  You are facing a door with a sign on it, as well as multiple items on the other walls. What should you do?")
 
+// Setup for answer to be used in loop
 let answer = ""
-answer = await ask('>_ ')
-let entryAnswer = ["read sign", "read", "look"]
+
   while(answer !== 'exit') {
-    
+    answer = await ask('>_ ')
     if (entryAnswer.includes(answer)){
      console.log("You selected " + answer + ". You walk over to the sign and read it. It states 'There is only 1 safe way out - if you choose poorly, you will meet your demise. Read carefully and choose wisely to get out of here....Alive!'")
       console.log('this works')
